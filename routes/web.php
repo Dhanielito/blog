396cc5c00744e139cabe22c8edc684c741ad0b86
Dhanielito <dhanielsantamaria@gmail.com>
2024-04-22 13:08:08 -0500

    actualizacion

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\CursoController;
+use App\Http\Controllers\HomeController;
+
+
+Route::get('/', HomeController::class);
+
+Route::get('curso', [CursoController::class, "index"]);
+
+Route::get('curso/create', [CursoController::class, "create"]);
+
+Route::get('curso/{curso}', [CursoController::class, "show"]);
